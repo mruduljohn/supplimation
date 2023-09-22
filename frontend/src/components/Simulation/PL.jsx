af4cd85f486a7dcaffaf1b8d07dba764066633a9
Jishnu M M <jishnumm4312@gmail.com>
2023-09-22 20:06:24 +0530

    grids

---
+++ frontend/src/components/Simulation/PL.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PL = () => {
+  return (
+    <div>PL</div>
+  )
+}
+
+export default PL
--- frontend/src/pages/Simulation.jsx
+++ frontend/src/pages/Simulation.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Products from '../components/Simulation/Products'
+import Simulator from '../components/Simulation/Simulator'
+import PL from '../components/Simulation/PL'
 
 function Simulation() {
   return (
-    <div>
-      
+    <div className=''>
+      <div className='grid grid-cols-1'>
+        <Products />
+      </div>
+      <div className='grid md:grid-cols-2 grid-cols-1'>
+        <Simulator />
+        <PL />
+      </div>
     </div>
   )
 }
